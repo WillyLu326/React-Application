570cea1fa813ca87dae0f0dfe10e943c20371f69
WillyLu326 <luzh0326@gmail.com>
2017-10-27 23:44:27 -0400

    create delete button

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,7 @@ import './Person.css';
 const Person = (props) => {
   return (
     <div className="Person">
+      <button className="deleteBtn">Delete</button>
       <p>My name is {props.name}.</p>
       <p>My age is {props.age}.</p>  
       <input /> 
