d4032dbdab8bd4e0ea08e82f4bfb5be7c7d8a98e
WillyLu326 <luzh0326@gmail.com>
2017-10-27 23:34:12 -0400

    create toggle persons button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { id: 0, name: 'Willy', age: 28 },
+      { id: 0, name: 'Zheng', age: 28 },
+      { id: 0, name: 'Cedar', age: 25 }
+    ],
+    showPersons: true
+  }
+
   render() {
     return (
       <div className="App">
         <button>Switch Name</button>
-
+        <button>Toggle Persons</button>
       </div>
     );
   }
