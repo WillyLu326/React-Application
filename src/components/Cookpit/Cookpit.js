432518d842d94318ee67c00c3a2e86d37466e6b1
WillyLu326 <luzh0326@gmail.com>
2017-10-29 23:17:01 -0400

    add css

--- src/components/Cookpit/Cookpit.js
+++ src/components/Cookpit/Cookpit.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Radium from 'radium';
+import './Cookpit.css'
 
 const Cookpit = (props) => {
 
@@ -21,12 +22,19 @@ const Cookpit = (props) => {
 		}
 	}
 
+	const titleClasses = [];
+	if (props.persons.length <= 2) {
+		titleClasses.push('blue');
+	}
+	if (props.persons.length <= 1) {
+		titleClasses.push('bold');
+	}
 
 	return (
 		<div>
 			<button style={switchBtnStyle} onClick={props.switch.bind(this, 'Willy Lu!!!')}>Switch Name</button>
 			<button style={toggleBtnStyle} onClick={props.toggle}>Toggle Persons</button>
-			<p>This is person List</p>
+			<p className={titleClasses.join(' ')}>This is person List</p>
 		</div>
 	);
 }
