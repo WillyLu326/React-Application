39dbcb69421873c4b42f513e6795f80ddd9d4441
WillyLu326 <luzh0326@gmail.com>
2017-10-29 23:04:30 -0400

    add css into Cookpit

--- src/components/Cookpit/Cookpit.js
+++ src/components/Cookpit/Cookpit.js
@@ -2,9 +2,21 @@ import React from 'react';
 
 const Cookpit = (props) => {
 
+	let switchBtnStyle = {
+		backgroundColor: 'green',
+		color: 'white',
+		padding: '16px',
+		cursor: 'pointer'
+	}
+
+	let toggleBtnStyle = {
+
+	}
+
+
 	return (
 		<div>
-			<button onClick={props.switch.bind(this, 'Willy Lu!!!')}>Switch Name</button>
+			<button style={switchBtnStyle} onClick={props.switch.bind(this, 'Willy Lu!!!')}>Switch Name</button>
 			<button onClick={props.toggle}>Toggle Persons</button>
 			<p>This is person List</p>
 		</div>
