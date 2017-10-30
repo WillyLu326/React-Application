3efb29c36dd3f5a9efaf15c7580dd6aeeb7bec84
WillyLu326 <luzh0326@gmail.com>
2017-10-29 20:44:00 -0400

    add query media

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,13 @@ import Radium from 'radium';
 import './Person.css';
 
 const Person = (props) => {
+  const style = {
+    '@media(min-width: 500px)': {
+      width: '450px'
+    }
+  }
   return (
-    <div className="Person">
+    <div className="Person" style={style}>
       <button className="deleteBtn" onClick={props.deleted}>Delete</button>
       <p>My name is {props.name}.</p>
       <p>My age is {props.age}.</p>  
