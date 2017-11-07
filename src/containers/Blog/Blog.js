484e6866eb628a4930178b47ebc2b9f08f8f92f9
WillyLu326 <luzh0326@gmail.com>
2017-11-06 22:15:57 -0500

    update code

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,28 +7,21 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div className="Blog">
-                <header>
-                    <nav>
-                        <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</Link></li>
-                        </ul>
-                    </nav>
-                </header>
-                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
-                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="Blog">
+				<header>
+					<nav>
+						<ul>
+							<li><a href="/"> Home </a></li>
+							<li><a href="new-post">New Post</a></li>
+						</ul>
+					</nav>
+				</header>
+			
+			</div>
+		);
+	}
 }
 
 export default Blog;
